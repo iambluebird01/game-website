bdbd08cfaeca4ea272a419f26f7086a223617b95
BlueBird <kirk.maningat@gmail.com>
2022-04-11 21:00:42 +0900

    GameInfo

--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -1,5 +1,80 @@
 import React from 'react';
-import styledComponents from 'styled-components';
+import styled from 'styled-components';
+import GameInfo1 from '../assets/buy-sell-1.png';
+import GameInfo2 from '../assets/buy-sell-2.png';
+import GameInfo3 from '../assets/buy-sell-3.png';
+
 export default function GameInfo() {
-  return <div></div>;
+  return (
+    <Section>
+      <div className="row">
+        <div className="col" id="gameInfo1">
+          <img src={GameInfo1} alt="Game Info 1" />
+          <div className="text">
+            <h2>
+              Game Info of Earthium <span>Structure</span> and
+              <span> Characters</span>
+            </h2>
+          </div>
+        </div>
+        <div className="col" id="gameInfo2">
+          <h2>
+            Buy and Sell
+            <span> Virtual Resources </span>
+            and Businesses
+          </h2>
+          <img src={GameInfo2} alt="Game Info 2" />
+        </div>
+        <div className="col" id="gameInfo3">
+          <img src={GameInfo3} alt="Game Info 3" />
+          <h2>
+            Earn <span>REKK</span>, a new kind of reality- backed cryptocurrency
+          </h2>
+        </div>
+      </div>
+    </Section>
+  );
 }
+
+const Section = styled.section`
+  margin: 6rem 10rem;
+  .row {
+    display: flex;
+    gap: 8rem;
+    flex-direction: column;
+    .col {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 2rem;
+      img {
+        height: 60vh;
+      }
+      h2 {
+        font-size: 3rem;
+        span {
+          color: var(--primary-color);
+        }
+      }
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    margin: 2rem 1rem;
+    .row {
+      gap: 2rem;
+      .col {
+        flex-direction: column;
+        text-align: center;
+        img {
+          height: 10rem;
+        }
+        h2 {
+          font-size: 1.5rem;
+        }
+        &:nth-of-type(2) {
+          flex-direction: column-reverse;
+        }
+      }
+    }
+  }
+`;
