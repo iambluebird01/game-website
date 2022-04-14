cf733f9659a66a64e94c824506e196f039bf86f5
BlueBird <kirk.maningat@gmail.com>
2022-04-14 09:56:51 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import Blogs from 'components/Blogs';
-import Footer from 'components/Footer';
-import GameInfo from 'components/GameInfo';
-import Home from 'components/Home';
-import Navbar from 'components/Navbar';
-import React from 'react';
+import React, { useEffect } from "react";
+import Blogs from "./components/Blogs";
+import GameInfo from "./components/GameInfo";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "components/ScrollToTop";
+import Designs from "components/Designs";
+import scrollreveal from "scrollreveal";
 
 export default function App() {
+  useEffect(() => {
+    const registerAnimations = () => {
+      const sr = scrollreveal({
+        origin: "bottom",
+        distance: "80px",
+        duration: 2000,
+        reset: false,
+      });
+      sr.reveal(
+        `
+        nav,
+        #home,
+        #gameInfo1,
+        #gameInfo2,
+        #gameInfo3,
+        #blogs,
+        footer
+    `,
+        {
+          opacity: 0,
+          interval: 500,
+        }
+      );
+    };
+    registerAnimations();
+  }, []);
+  window.setTimeout(() => {
+    const home = document.getElementById("home");
+    home.style.transform = "none";
+  }, 1500);
   return (
     <div>
+      <ScrollToTop />
       <Navbar />
       <Home />
+      <Designs />
       <GameInfo />
       <Blogs />
       <Footer />
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,31 +1,40 @@
-import React from 'react';
-import styled from 'styled-components';
-import { BsArrowRightShort } from 'react-icons/bs';
-export default function Button(props) {
-  let { icon, text } = props;
-  if (icon === undefined) icon = <BsArrowRightShort />;
+import React, { useState } from "react";
+import styled from "styled-components";
+import { AiOutlineArrowUp } from "react-icons/ai";
+export default function () {
+  const [visible, setVisible] = useState(false);
+  window.addEventListener("scroll", () => {
+    window.pageYOffset > 100 ? setVisible(true) : setVisible(false);
+  });
   return (
-    <Btn>
-      {icon}
-      {text}
-    </Btn>
+    <Div>
+      <a href="#" className={`${visible ? "block" : "none"}`}>
+        <AiOutlineArrowUp />
+      </a>
+    </Div>
   );
 }
 
-const Btn = styled.button`
-  background-color: var(--primary-color);
-  padding: 1rem 2rem;
-  border-radius: 0.3rem;
-  border: none;
-  outline: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
-  font-weight: bold;
-  font-size: 25px;
-  cursor: pointer;
-  svg {
-    font-size: 2rem;
+const Div = styled.div`
+  .none {
+    opacity: 0;
+    visibility: hidden;
+  }
+  a {
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+    background-color: var(--primary-color);
+    padding: 1rem;
+    border-radius: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: 0.4s ease-in-out;
+    z-index: 1;
+    svg {
+      color: black;
+      font-size: 1.3rem;
+    }
   }
 `;
