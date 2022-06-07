0f8348e1c77e5df92eae3037ff4080b144328dc7
BlueBird <kirk.maningat@gmail.com>
2022-06-07 21:40:32 +0900

    fixed

--- backend/passport.js
+++ backend/passport.js
@@ -3,9 +3,12 @@ const GithubStrategy = require('passport-github2').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const passport = require('passport');
 
-const GOOGLE_CLIENT_ID =
-  '988714366879-fn365hn3b6560o5ik1hr8ojuvvt76q1d.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
+const GOOGLE_CLIENT_ID = require('dotenv').config({
+  path: 'C:UsersgoodboyDocumentsGitHubgame-website\backend.env',
+});
+const GOOGLE_CLIENT_SECRET = require('dotenv').config({
+  path: 'C:UsersgoodboyDocumentsGitHubgame-website\backend.env',
+});
 
 passport.use(
   new GoogleStrategy(
